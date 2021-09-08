a48725ec15286ed57e7ccb2b119db63244bd60b1
start20201202 <start20201202@gmail.com>
2021-09-08 16:30:50 +0300

    create index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./16_api/containers/news/news";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
